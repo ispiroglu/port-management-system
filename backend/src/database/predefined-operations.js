f90fc4e408e1db25c5714037b5fe935022c8afad
Gürol Berkay Çınar <gurolberkaycinar@gmail.com>
2023-01-17 16:03:49 +0300

    column name response fix

--- backend/src/database/predefined-operations.js
+++ backend/src/database/predefined-operations.js
@@ -4,60 +4,90 @@ const predefinedQuery = require('../util/predefined-queries');
 async function intersectOldMerchantWorkes() {
     let query = predefinedQuery.INTERSECT_OLD_MERCHANT_WORKERS;
     var response = await client.query(query);
+    let columns = response.fields
+    const columnNames = columns.map(columns => columns.name);
     return {
         query: query,
         rows: response.rows,
-        columns: response.fields
+        columns: columnNames
     }
 }
 
 async function unionPrivateShipOwnersOrYoungShipOwners() {
     let query = predefinedQuery.UNION_PRIVATE_SHIP_OWNERS_OR_YOUNG_SHIP_OWNERS;
     var response = await client.query(query);
+    let columns = response.fields
+    const columnNames = columns.map(columns => columns.name);
     return {
         query: query,
         rows: response.rows,
-        columns: response.fields
+        columns: columnNames
     }
 }
 
 async function exceptPrivateButTaxfreeShips() {
     let query = predefinedQuery.EXCEPT_PRIVATE_BUT_TAXFREE_SHIPS;
     var response = await client.query(query);
+    let columns = response.fields
+    const columnNames = columns.map(columns => columns.name);
     return {
         query: query,
         rows: response.rows,
-        columns: response.fields
+        columns: columnNames
     }
 }
 
 async function havingShipsThatHaveMinOneWorker() {
     let query = predefinedQuery.HAVING_SHIPS_THAT_HAVE_MIN_ONE_WORKER;
     var response = await client.query(query);
+    let columns = response.fields
+    const columnNames = columns.map(columns => columns.name);
     return {
         query: query,
         rows: response.rows,
-        columns: response.fields
+        columns: columnNames
     }
 }
 
 async function servantCount() {
     let query = predefinedQuery.AGGR_PUBLIC_SERVANT_COUNT;
     var response = await client.query(query);
+    let columns = response.fields
+    const columnNames = columns.map(columns => columns.name);
     return {
         query: query,
         rows: response.rows,
-        columns: response.fields
+        columns: columnNames
     }
 }
 
 async function getPrivateShipOwners() {
     let query = predefinedQuery.VIEW_PRIVATE_SHIP_OWNERS;
     var response = await client.query(query);
+    let columns = response.fields
+    const columnNames = columns.map(columns => columns.name);
     return {
         query: query,
         rows: response.rows,
-        columns: response.fields
+        columns: columnNames
+    }
+}
+
+async function getAvgWorkerAge(shipType) {
+    let query = `select avg_age(${shipType})`;
+    var response = await client.query(query);
+    return {
+        query: query,
+        rows: response.rows
+    }
+}
+
+async function getShipPowerLengthFilter(power, length) {
+    let query = `select numf_of_filtered_ships_by_power_and_length(${power},${length})`;
+    var response = await client.query(query);
+    return {
+        query: query,
+        rows: response.rows
     }
 }
 
@@ -67,5 +97,7 @@ module.exports = {
     exceptPrivateButTaxfreeShips,
     havingShipsThatHaveMinOneWorker,
     servantCount,
-    getPrivateShipOwners
+    getPrivateShipOwners,
+    getAvgWorkerAge,
+    getShipPowerLengthFilter
 }
